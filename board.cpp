2a897790b69058da433ea492552262211e3fb6e7
jwittneb <jwittneb@cs.uwaterloo.ca>
2018-03-14 21:09:07 -0400

    Just cleaning things up, mostly

--- board.cpp
+++ board.cpp
@@ -1,24 +1,35 @@
 #include "board.h"
 
-Board::Board() {
-	tiles[0][0]=bluePawn;
-	tiles[0][1]=bluePawn;
-	tiles[0][2]=blueKing;
-	tiles[0][3]=bluePawn;
-	tiles[0][4]=bluePawn;
 
-	tiles[4][0]=redPawn;
-	tiles[4][1]=redPawn;
-	tiles[4][2]=redKing;
-	tiles[4][3]=redPawn;
-	tiles[4][4]=redPawn;
+//constructor sets up the board to the starting game position
+Board::Board() {
+	//setting up the blue/red
+	for (int i=0; i<boardWidth; ++i) {
+		tiles[0][i] = bluePawn;
+		tiles[boardHeight-1][i] = redPawn;
+	}
+	tiles[0][thronePos] = blueKing;
+	tiles[boardHeight-1][thronePos] = redKing;
+	
+	//make sure that the rest of the board is empty
+	for (int i=1; i<boardHeight-1; ++i) {
+		for (int j=0; j<boardWidth; ++j) {
+			tiles[i][j]=empty;
+		}
+	}
 }
 
 TileObject Board::get_tile_object(int row, int col) {
 	return tiles[row][col];
 }
 
+//Function to move pieces, does not do checking that you are moving your own pieces, which should be
+//checked in the player move functions
 void Board::move_piece(int initrow, int initcol, int endrow, int endcol) {
-	tiles[endrow][endcol] = tiles[initrow][initcol];
-	tiles[initrow][initcol] = empty;
+	if (tiles[initrow][initcol]==empty) {
+		cerr << "Attempting to move from an empty location" << endl;	
+	} else {
+		tiles[endrow][endcol] = tiles[initrow][initcol];
+		tiles[initrow][initcol] = empty;
+	}
 }
--- board.h
+++ board.h
@@ -3,13 +3,16 @@
 //#include "tile.h"
 #include <vector>
 
+const int boardHeight = 5;
+const int boardWidth = 5;
+const int thronePos = 2;
+
 enum TileObject {empty, redPawn, redKing, bluePawn, blueKing};
 
 class Board {
-		TileObject tiles[5][5]; //change this, eventually
-//		Tile tiles[5][5];
+		TileObject tiles[boardHeight][boardWidth]; //change this, eventually
 	public:
-		void move_piece(int init_row, int init_col, int end_row, int end_col);
+		void move_piece(int initRow, int initCol, int endRow, int endCol);
 
 		TileObject get_tile_object(int row, int col);	
 //		static Board* getInstance;
